1681051941fbe0f603c9e2948334902ae8d92efc
fieldOfView <aldo@fieldofview.com>
2022-04-03 09:37:52 -0700

    Create a .curapackage for each supported major SDK version

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ const path = require('path');
 
 const archiver = require('archiver');
 
+let packageFiles = [];
+
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    //const pluginId = core.getInput('plugin_id');
-    //const sourcePath = core.getInput('source_folder');
-    //let packageInfoPath = core.getInput('package_info_path')
-    const pluginId = 'OctoPrintPlugin';
-    const sourcePath = '../OctoPrintPlugin';
-    let packageInfoPath = '';
-
-
-    const archiveFileName = pluginId + '.curapackage';
+    const sourcePath = core.getInput('source_folder');
+    let pluginId = core.getInput('plugin_id');
+    let packageInfoPath = core.getInput('package_info_path')
+    
     const staticFilePath = fs.existsSync(__dirname + "/files") ? __dirname + "/files" : __dirname + "/dist/files";
 
     // get package info, or load "empty" package info template
-    if (!fs.existsSync(packageInfoPath)){
+    if (packageInfoPath == "" || fs.existsSync(packageInfoPath)){
       packageInfoPath = staticFilePath + "/package.json";
     }
 
@@ -45,72 +42,100 @@ async function run() {
       throw err;
     }
 
+    if (pluginId == "") {
+      pluginId = packageInfo["package_id"];
+    }
 
-    // create a file to stream archive data to.
-    const output = fs.createWriteStream(archiveFileName);
-    const archive = archiver('zip', {
-      zlib: { level: 9 } // Sets the compression level.
-    });
+    const pluginVersion = pluginInfo["version"]
 
-    // listen for all archive data to be written
-    // 'close' event is fired only when a file descriptor is involved
-    output.on('close', function() {
-      console.log(archive.pointer() + ' total bytes');
-      console.log('archiver has been finalized and the output file descriptor has closed.');
-    });
+    core.info(`Creating curapackages for ${pluginId} ${pluginVersion}...`);
 
-    // This event is fired when the data source is drained no matter what was the data source.
-    // It is not part of this library but rather from the NodeJS Stream API.
-    // @see: https://nodejs.org/api/stream.html#stream_event_end
-    output.on('end', function() {
-      console.log('Data has been drained');
-    });
+    // copy plugin info into package info
+    packageInfo["package_id"] = pluginId;
+    packageInfo["package_version"] = pluginVersion;
+    packageInfo["display_name"] = pluginInfo["name"];
+    if (packageInfoPath == staticFilePath + "/package.json") {
+      packageInfo["description"] = pluginInfo["description"];
+    }
 
-    // good practice to catch warnings (ie stat failures and other non-blocking errors)
-    archive.on('warning', function(err) {
-      if (err.code === 'ENOENT') {
-        // log warning
-      } else {
-        // throw error
+    const supportedSDKVersions = pluginInfo["supported_sdk_versions"];
+    const majorSDKVersions = new Set(supportedSDKVersions.map(function (versionString) {
+      return parseInt(versionString.split(".")[0]);
+    }));
+
+    majorSDKVersions.forEach(function(majorVersion) {
+      const semanticVersion = majorVersion + ".0.0";
+      const archiveFileName = pluginId + "_" + pluginInfo["version"] + "_" + semanticVersion + ".curapackage";
+      packageFiles.push(archiveFileName);
+
+      packageInfo["sdk_version"] = majorVersion;
+      packageInfo["sdk_version_semver"] = semanticVersion;
+
+      // create a file to stream archive data to.
+      const output = fs.createWriteStream(archiveFileName);
+      const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+      });
+
+      // listen for all archive data to be written
+      // 'close' event is fired only when a file descriptor is involved
+      output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+      });
+
+      // This event is fired when the data source is drained no matter what was the data source.
+      // It is not part of this library but rather from the NodeJS Stream API.
+      // @see: https://nodejs.org/api/stream.html#stream_event_end
+      output.on('end', function() {
+        console.log('Data has been drained');
+      });
+
+      // good practice to catch warnings (ie stat failures and other non-blocking errors)
+      archive.on('warning', function(err) {
+        if (err.code === 'ENOENT') {
+          // log warning
+        } else {
+          // throw error
+          throw err;
+        }
+      });
+
+      // good practice to catch this error explicitly
+      archive.on('error', function(err) {
         throw err;
-      }
+      });
+
+      // pipe archive data to the file
+      archive.pipe(output);
+
+      archive.directory(
+        sourcePath,
+        'files/plugins/' + pluginId,
+        file => file.name.startsWith('.git') ? false : file
+      );
+
+      archive.append(
+        JSON.stringify(packageInfo),
+        {name: 'package.json'}
+      );
+
+      archive.file(
+        staticFilePath + "/[Content_Types].xml",
+        {name: "[Content_Types].xml"}
+      );
+
+      archive.directory(
+        staticFilePath + "/_rels",
+        '_rels'
+      );
+
+      // finalize the archive (ie we are done appending files but streams have to finish yet)
+      // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+      archive.finalize();
     });
 
-    // good practice to catch this error explicitly
-    archive.on('error', function(err) {
-      throw err;
-    });
-
-    // pipe archive data to the file
-    archive.pipe(output);
-
-    archive.directory(
-      sourcePath,
-      'files/plugins/' + pluginId,
-      file => file.name.startsWith('.git') ? false : file
-    );
-
-    archive.append(
-      JSON.stringify(packageInfo),
-      {name: 'package.json'}
-    );
-
-    archive.file(
-      staticFilePath + "/[Content_Types].xml",
-      {name: "[Content_Types].xml"}
-    );
-
-    archive.directory(
-      staticFilePath + "/_rels",
-      '_rels'
-    );
-
-    // finalize the archive (ie we are done appending files but streams have to finish yet)
-    // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-    archive.finalize();
-
-
-    //core.setOutput('time', new Date().toTimeString());
+    core.setOutput('packages', packageFiles);
   } catch (error) {
     core.setFailed(error.message);
   }
